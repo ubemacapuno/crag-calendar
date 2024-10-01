b6064cf9aff2d626d03d916f2c3f20a6a327a18e
Fast DMS Corey <corey.damocles@fastdms.com>
2024-10-01 14:13:19 -0700

    Updated climb schema - WIP

--- src/db/schema/climbs.ts
+++ src/db/schema/climbs.ts
@@ -1,8 +1,34 @@
 import { relations } from "drizzle-orm";
-import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
 
 import users from "./users";
+
+const vScaleBoulderingGrades = [
+  "V0-",
+  "V0",
+  "V1",
+  "V2",
+  "V3",
+  "V4",
+  "V5",
+  "V6",
+  "V7",
+  "V8",
+  "V9",
+  "V10",
+  "V11",
+  "V12",
+  "V13",
+  "V14",
+  "V15",
+  "V16",
+  "V17",
+  "V17+",
+] as const;
+
+// TODO: Add "Font" bouldering scale too ?
 
 const climbs = pgTable("climb", {
   id: uuid("id").defaultRandom().primaryKey(),
@@ -10,7 +36,7 @@ const climbs = pgTable("climb", {
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
   createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
-  grade: varchar("grade", { length: 4 }).notNull(), // V0 to V17+
+  grades: text("grades").notNull().$type<string[]>(), // Store as JSON array
 });
 
 export const climbsRelations = relations(climbs, ({ one }) => ({
@@ -20,7 +46,9 @@ export const climbsRelations = relations(climbs, ({ one }) => ({
   }),
 }));
 
-export const InsertClimbSchema = createInsertSchema(climbs).omit({
+export const InsertClimbSchema = createInsertSchema(climbs, {
+  grades: z.array(z.enum(vScaleBoulderingGrades)).nonempty(),
+}).omit({
   id: true,
   userId: true,
   createdAt: true,
